ec3c48f457af8cc0029c4ef25875e201325cc03d
mhs <mhs@MHS-Science2-10>
2017-10-11 17:04:12 -0700

    mrt

---
+++ statement practice/src/practice.java
@@ -0,0 +1,22 @@
+
+public class practice {
+	public static void main(String[] args){
+		int x=5;
+		int y=9;
+		int p=16;
+		if(y>x && y > p){
+			System.out.println(y);
+		}
+		if(p>y && p > x){
+			//PRINT P
+		}
+		if (x>y && x > p){
+			//print x
+		}
+		
+		while(x<8){
+			System.out.println("go programing");
+			x++;
+		}
+	}
+}
